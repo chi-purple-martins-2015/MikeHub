95baaafc55b836f09487d133647228a9d013f218
CorainChicago <chaysmagan@gmail.com>
2015-11-11 18:16:04 -0600

    Fix rspec setup - add new article controller and view code
    Added the .rspec file, ran rails generate rspec:install

--- mikehub/app/controllers/articles_controller.rb
+++ mikehub/app/controllers/articles_controller.rb
@@ -1,7 +1,28 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :show]
-  
+
   def new
   end
 
+  def create
+    @article = Article.create(article_params)
+    if @article.valid?
+      redirect_to root_path
+    else
+      @errors = @article.errors.full_messages
+      render "/articles/new"
+    end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :subtitle, :body)
+  end
+
 end
+

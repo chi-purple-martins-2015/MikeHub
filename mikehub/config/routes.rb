72d0412703d8e8690e79d204d4b1b2ab6930b85e
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-12 10:27:47 -0600

    start adding search routes

--- mikehub/config/routes.rb
+++ mikehub/config/routes.rb
@@ -23,7 +23,9 @@ Rails.application.routes.draw do
 
   get 'articles/:id' => 'articles#show'
 
+  get "search/:search_term" => "pages#search"
 
+  post "search/"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

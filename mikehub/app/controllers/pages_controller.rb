e888c82987a190df3b31e50d38ab5c8cd6c6c85b
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-12 10:27:37 -0600

    start writing search method

--- mikehub/app/controllers/pages_controller.rb
+++ mikehub/app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
     @featured_articles = Article.limit(8)
     @recently_added_articles = Article.order(created_at: :desc).limit(5)
   end
+
+  def search
+    
+  end
 end

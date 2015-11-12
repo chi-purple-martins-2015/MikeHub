f75e2101fead43f48a4b4ac44dc692b09a8310c2
josue aceves <josueaceves.ja@gmail.com>
2015-11-12 11:21:26 -0600

    fix deprication warining and add sessions controller test

--- mikehub/spec/models/user_spec.rb
+++ mikehub/spec/models/user_spec.rb
@@ -4,11 +4,14 @@ require 'rails_helper'
  describe User do
     it "should have many articles" do
       user = User.reflect_on_association(:articles)
-      user.macro.should == :has_many
+      expect(user.macro).to eq(:has_many)
+      # user.macro.should == :has_many
     end
     it "should have many edits" do
       user = User.reflect_on_association(:edits)
-      user.macro.should == :has_many
+      # user.macro.should == :has_many
+      expect(user.macro).to eq(:has_many)
     end
 
 end
+

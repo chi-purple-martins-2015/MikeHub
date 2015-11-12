47dcd21e3ca3b6e0013a4bd5b5c0d0816d18ec31
[01;31m[K"name"[m[K: "McEileen <[01;31m[K"email"[m[K: null>
2015-11-11 20:46:00 -0600

    write one failing test

--- mikehub/spec/views/user_view_spec.rb
+++ mikehub/spec/views/user_view_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
-describe "User View" do
-  @user_params = {
-    first_name: "Joe",
-    last_name: "Jefferson",
-    email: "joe@thejeffersons.org",
-    img_url: "http://a-z-animals.com/media/animals/images/original/capybara3.jpg",
-    password: "password"
-  }
-  @user = User.new(@user_params)
-  expect(page).to have_content('Joe')
-end
+feature 'User visits profile' do
+  scenario "sees a list of article titles" do
+    user_params = {first_name: "Joe",
+                  last_name: "Jefferson",
+                  email: "joe@thejeffersons.org",
+                  img_url: "http://www.rainforest-alliance.org/sites/default/files/uploads/4/capybara-family_15762686447_f9f8a0684a_o.jpg",
+                  password: "password"
+    }
 
+    @user = User.create(user_params)
+    visit '/users/1'
+
+    page.assert_selector('li.article', :count => 2)
+
+
+
+  end
+end

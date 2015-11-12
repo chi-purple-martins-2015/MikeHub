91de571dbf7d981a7b2911cfce5619f4510078fa
[01;31m[K"name"[m[K: "McEileen <[01;31m[K"email"[m[K: null>
2015-11-11 18:20:42 -0600

    add route for individ profile page

--- mikehub/config/routes.rb
+++ mikehub/config/routes.rb
@@ -9,8 +9,7 @@ Rails.application.routes.draw do
   get 'users/delete'
 
 
-  get 'users/show'
-
+  get 'users/:id' => 'users#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

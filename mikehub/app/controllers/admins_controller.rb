f7239fbf25dce377274e818f7f44bf8e3a89e39b
CorainChicago <chaysmagan@gmail.com>
2015-11-13 13:47:00 -0600

    format the regex

--- mikehub/app/controllers/admins_controller.rb
+++ mikehub/app/controllers/admins_controller.rb
@@ -16,3 +16,21 @@ class AdminsController < ApplicationController
 
 
 end
+
+/(\r\n){2}<p>([\w ]*\w[\w ])<\/p>/
+
+heading_matcher = /((\r\n){2}<p>)([\w ]*\w[\w ])(<\/p>(\r\n){2})/
+
+link_open_tag = '<a href="<link>">'
+link_close_tag = '</a>'
+
+replacement_text = '\1' + link_open_tag +'\3' + link_close_tag + '\4'
+
+article_text.gsub(heading_matcher, replacement_text)
+
+"</p>(\r\n\r\n<p>)(The Bears2)(</p>\r\n\r\n)<p>"
+
+"</p>\r\n\r\n<p><a href="<link>">The Bears2</a></p>\r\n\r\n<p>"
+
+
+
--- mikehub/app/helpers/articles_helper.rb
+++ mikehub/app/helpers/articles_helper.rb
@@ -3,4 +3,15 @@ module ArticlesHelper
   def short_body(string)
     string[0..160]
   end
+
+  def find_subheading(article)
+
+    heading_matcher = /((\r\n){2}<p>)([\w ]*\w[\w ])(<\/p>(\r\n){2})/
+    link = '\3'
+    link.gsub(/\s+/, "")
+    link_open_tag = '<a name="\3" href="/articles/'+ article.id.to_s + link +'>'
+    link_close_tag = '</a>'
+    replacement_text = '\1' + link_open_tag +'\3' + link_close_tag + '\4'
+    article.content.gsub(heading_matcher, replacement_text)
+  end
 end

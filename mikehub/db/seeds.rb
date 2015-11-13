090dec28eb2745b67487b82f3a3b78f7b3b4675a
Eileen McFarland <eileenm@gmail.com>
2015-11-13 11:34:36 -0600

    setup admin dashboard, only routes to users index

--- mikehub/db/seeds.rb
+++ mikehub/db/seeds.rb
@@ -2,11 +2,11 @@
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.user_name + "@dbc.com", admin: true, img_url: Faker::Avatar.image, password: "kaboom")
 end
 
-# 10.times do
-#   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.user_name + "@dbc.com", admin: false, img_url: Faker::Avatar.image, password: "password")
-# end
+10.times do
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.user_name + "@dbc.com", admin: false, img_url: Faker::Avatar.image, password: "password")
+end
 
-# 30.times do
-#   User.find(rand(1..10)).articles << Article.new(title: Faker::App.name, subtitle: Faker::Company.catch_phrase, content: Faker::Lorem.paragraphs(rand(6..20)).join(", "))
-# end
+30.times do
+  User.find(rand(1..10)).articles << Article.new(title: Faker::App.name, subtitle: Faker::Company.catch_phrase, content: Faker::Lorem.paragraphs(rand(6..20)).join(", "))
+end
 

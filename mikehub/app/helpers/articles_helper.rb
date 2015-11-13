bf3b19334324cbaf1c3e8bfe458f49a1acca64f1
CorainChicago <chaysmagan@gmail.com>
2015-11-13 14:45:31 -0600

    push links for subsections

--- mikehub/app/helpers/articles_helper.rb
+++ mikehub/app/helpers/articles_helper.rb
@@ -4,17 +4,11 @@ module ArticlesHelper
     string[0..160]
   end
 
-  def get_subheading_link(article)
-    sub_heading_location = article[/((\r\n){2}<p>)([\w ]*\w[\w ])(<\/p>(\r\n){2})/]
-    subheading = sub_heading_location[/([\w ]*\w[\w ])/]
-    subheading.delete!(' ')
-  end
-
   def find_subheading(article)
-    get_subheading_link(article)
     heading_matcher = /((\r\n){2}<p>)([\w ]*\w[\w ])(<\/p>(\r\n){2})/
     link = '\3'
-    link_open_tag = '<a name="\3" href="/articles/'+ article.id.to_s + '#\3">'
+    p link
+    link_open_tag = '<a name="\3" href="/articles/' + article.id.to_s + '/#\3">'
     link_close_tag = '</a>'
     replacement_text = '\1' + link_open_tag +'\3' + link_close_tag + '\4'
     article.content.gsub(heading_matcher, replacement_text)

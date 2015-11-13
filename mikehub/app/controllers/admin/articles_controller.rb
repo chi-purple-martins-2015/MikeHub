9972d718d45b3ad530923cac9b894cf6f603ffbf
josue aceves <josueaceves.ja@gmail.com>
2015-11-13 14:51:47 -0600

    remove underline

--- mikehub/app/controllers/admin/articles_controller.rb
+++ mikehub/app/controllers/admin/articles_controller.rb
@@ -1,1 +1,15 @@
-articles_controller.rb
+class Admin::ArticlesController < ApplicationController
+  before_filter :authorized?
+
+
+  private
+
+  def authorized?
+    unless admin_logged_in?
+      flash[:error] = "You are not authorized to view that page."
+      redirect_to root_path
+    end
+  end
+
+
+end
